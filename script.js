a5dca165c966f3eaceb5ef31e4a99a3670be39d2
Sparsh Gupta <sparshgupta.a8@gmail.com>
2017-12-07 08:30:29 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+var t=prompt("What's your name?");
+alert("Hi nice meeting you "+t);
+console.log("dhsgfhsdgh " + t );
---
+++ script1.js
@@ -0,0 +1,7 @@
+var first=prompt("What's your first name?");
+var second=prompt("What's your last name?");
+var age=prompt("What's your age?");
+alert(first + " " + second );
+alert(age);
+console.log(first+ ' '+ second);
+console.log(age);
